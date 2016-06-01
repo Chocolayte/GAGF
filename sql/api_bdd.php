47fc8a062e360526f9f67ef8b224543c97e9d7fb
Chocolayte <ludovic.baudin@utbm.fr>
2016-06-01 15:05:58 +0200

    add insee plus add competence api_bdd

--- sql/api_bdd.php
+++ sql/api_bdd.php
@@ -19,6 +19,25 @@ class BDD {
     }
   }
   
+  // Fonction pour ajouter un utilisateur
+  function addUtilisateur($vID,$vMail,$vPass,$vNom,$vPrenom,$vType,$vAdresse,$vCodePostal,$vVille,$vTel,$vActive) {
+  	$sql_req = "INSERT INTO UTILISATEUR VALUES(".$vID.",'".$vMail."','".$vPass."','".$vNom."','".$vPrenom."',".$vType.",'".$vAdresse."','".$vCodePostal."','".$vVille."','".$vTel."',".$vActive.");";
+    $this->sendRequest($sql_req);
+  }
+  
+  // Fonction pour sélectionner tous les utilisateurs
+  function selectUtilisateurs() {
+    $sql_req = "SELECT * FROM UTILISATEUR";
+    $this->sendRequest($sql_req);
+  }
+  
+  // Fonction d'envoi d'une requête
+  function sendRequest($req) {
+    $reqReturn = $this->bdd->query($req);
+    print_r($reqReturn->fetchAll(PDO::FETCH_ASSOC));
+    return $reqReturn->fetchAll(PDO::FETCH_ASSOC);
+  }
+  
   function dataExist($table, $colonne, $test)
   {
 	$sql = "SELECT COUNT($colonne) AS dejaexistant FROM $table WHERE $colonne = '$test'";
@@ -32,6 +51,16 @@ class BDD {
 	if(!$this->dataExist("competence", "COMPETENCE_LIBELLE", "$competence")){
 		$sql = "INSERT INTO COMPETENCE VALUES(NULL, '$competence', $bydefault)";
 		//echo $sql;
+		$this->bdd->query($sql);
+		return true;
+	}
+	else return false;
+  }
+  function AddInsee($code, $libelle)
+  {
+	if(!$this->dataExist("classification", "CLASSIFICATION_CODE_INSEE", "$code")){
+		$sql = "INSERT INTO CLASSIFICATION VALUES(NULL, '$code', '$libelle')";
+		echo $sql;
 		$this->bdd->query($sql);
 		return true;
 	}
