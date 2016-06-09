a577cf5c9d8e76038bc9cb950125614310cca114
aymsdev <aymeric.chagrot@gmail.com>
2016-06-09 11:41:00 +0200

    Write values in calendar

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html> 
 <?php
-  if (!isset($_COOKIE["log"]))
-	header("Location: index.php");
+    if (!isset($_COOKIE["log"]))
+  	  header("Location: index.php");
 
 	$css = "style/monthly.css"; 
 	$header = "Accueil"; 
@@ -9,18 +9,52 @@
 	include('include/main.php'); 
 ?>
 
-<div class="mdl-grid demo-content">
-    <div class="demo-charts mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid" >
-		<div class="monthly" id="mycalendar"></div>
-	</div>
-</div>
+
+<main class="mdl-layout__content">
+  <div class="mdl-grid demo-content">
+      <h3>Planning</h3>
+      
+      <div class="mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid" >
+  		    <div class="monthly" id="mycalendar"></div>
+  	  </div>
+      <h3>Gérer ses formations</h3>
+      <?php
+        // récupération des formations
+        $dbhost = 'localhost';
+        $dbuser = 'root';
+        $dbpassword = $_SERVER['SERVER_NAME'] == 'gagf.mypix.ovh' ? 'Sq$pcA9e!!' : 'root';
+        $dbname = 'gagf' ;
+      
+        $bdd = new PDO("mysql:host=$dbhost;dbname=$dbname", "$dbuser", "$dbpassword");
+        $sql = "SELECT A.FORMATION_INTITULE, B.FD_DATE_DEBUT, B.FD_DATE_FIN FROM FORMATION A INNER JOIN FORMATION_DATE B ON A.FORMATION_ID = B.FD_FORMATION";
+        foreach  ($bdd->query($sql) as $row) {
+          print $row[0] . "\t";
+          print  $row[1] . "\t";
+          print $row[2] . "<br/>";
+        }
+
+      ?>
+  </div>
+</main>
 
 <script type="text/javascript" src="./js/jquery.js"></script>
 <script type="text/javascript" src="./js/monthly.js"></script>
 <script type="text/javascript">
+<<<<<<< Updated upstream
 	$(window).load( function() {
 		$('#mycalendar').monthly();
 	});
 </script>
+<script>
+    $(window).load( function() {
+        $('#mycalendar').monthly({
+          mode: 'event',
+          xmlUrl: 'events.xml'
+        });
+    });
+</script>
+
+
+
 
 <?php include('include/footer.php'); ?>
