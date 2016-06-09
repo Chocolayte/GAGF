41b8bcce12037da075ff418a79435bad1d4c80f6
jjakcr <jean-jacques.ropp@utbm.fr>
2016-06-09 16:05:55 +0200

    mise en forme formation.php

--- formation.php
+++ formation.php
@@ -32,36 +32,55 @@ if (!isset($_COOKIE["log"]))
   <div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
 	<form method="post" action="add_formation.php">
 		<p>
-			<label for="Nom">Intitulé :</label>
-			<input type="text" name="nom" id="nom" placeholder="Nom de la formation" size="40" maxlength="50" /> 
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">	
+			<label class="mdl-textfield__label" for="nom">Intitulé</label>
+			<input class="mdl-textfield__input" type="text" name="nom" id="nom" size="40" maxlength="50" /> 
+			
+		</div>
 		</p>
-		<p>
-			<label for="Type">Type :</label>
-			<select>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">	
+			<label class="mdl-textfield__label" for="Type">Type</label>
+			<select class="mdl-textfield__input" id="Type" name="Type">
+				<option value="">&nbsp;
 			   <option>1 - Intra-entreprise</option>
 			   <option>2 - Inter-entreprise</option>
 			   <option selected="selected">3 - Sous-traitance</option>
 			</select> 
-		</p>
-		<p>
-			<label for="Type">Classification INSEE :</label>
-			<select>
-			   <option>1 - Programmation</option>
-			   <option>2 - Réseau</option>
-			   <option selected="selected">3 - Glandouille</option>
+		</div>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">	
+			<label class="mdl-textfield__label" for="insee">Classification INSEE</label>
+			<select class="mdl-textfield__input" id="insee" name="insee">
+				<option value="">&nbsp;
+			   <?php
+						$result=$bdd->GetClass();
+						$cmp = count($result);
+						for($i=0;$i<$cmp;$i++){	
+							$value=$result[$i]["CLASSIFICATION_CODE_INSEE"]." - ".$result[$i]["CLASSIFICATION_LIBELLE"];
+							echo "<option value=\"$value\">$value</option>";
+							}
+					?>
 			</select> 
+		</div>
+		<p>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">	
+		<label class="mdl-textfield__label" for="date">Sélectionner Date</label>
+			<input class="mdl-textfield__input" type="text" id="datepicker" >
+		</div>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">	
+			<label class="mdl-textfield__label" for="duree">Durée en jours</label>
+			<input class="mdl-textfield__input" type="number " name="jour" id="duree"  size="20" maxlength="50" />
+		</div>
 		</p>
 		<p>
-		<label for="Nom">Date :</label>
-			<input type="text" id="datepicker" placeholder="Sélectionner une date">
-			<label for="Nom">Durée :</label>
-			<input type="number " name="jour" id="nom" placeholder="Nombre de jours" size="20" maxlength="50" />
-		</p>
-		<p>
-			<label for="Nom">Heure de début:</label>
-			<input type="text" id="timepicker" name="timepicker" class="timepicker" placeholder="Sélectionner une heure">
-			<label for="Nom">Heure de fin:</label>
-			<input type="text" id="timepicker2" name="timepicker2" class="timepicker" placeholder="Sélectionner une heure">
+		
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<label class="mdl-textfield__label" for="timepicker">Sélectionner une heure de début</label>
+			<input class="mdl-textfield__input" type="text" id="timepicker" name="timepicker" class="timepicker">
+		</div>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<label class="mdl-textfield__label" for="timepicker2">Sélectionner une heure de fin</label>
+			<input class="mdl-textfield__input" type="text" id="timepicker2" name="timepicker2" class="timepicker">
+		</div>
 		</p>
 		<p align="right">
 			<input class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color-text--white" type="submit" value="Ajouter une date" />
